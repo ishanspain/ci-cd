e8a0c8ac802a128e8c01f95212b4534861aa6896
Ishan Sharma <ishan.spain@gmail.com>
2026-09-23 17:36:13 +0530

    fix: update file transfer and restart commands in vm.sh

--- vm.sh
+++ vm.sh
@@ -7,8 +7,31 @@
 # ssh pcvm \
 # "ls"
 
-scp -r ./tests pcvm:/home/ubuntu/
-ssh pcvm "/home/ubuntu/.local/share/pnpm/bin/pm2 restart app"
+# scp -r ./tests pcvm:/home/ubuntu/
+# ssh pcvm "/home/ubuntu/.local/share/pnpm/bin/pm2 restart app"
 
 
-# ssh pcvm "sudo -u admin pm2 restart app"
+# =======================
+
+
+ssh pcvm "
+sudo -u admin cp -r /home/ubuntu/tests /home/admin/ &&
+sudo -u admin /home/admin/.local/share/pnpm/bin/pm2 restart app
+"
+
+# =======================
+
+
+PASSWORD='your-password'
+
+sshpass -p "$PASSWORD" \
+scp -r ./tests admin@3.6.105.65:/home/admin/
+
+sshpass -p "$PASSWORD" \
+ssh admin@3.6.105.65 \
+"/home/admin/.local/share/pnpm/bin/pm2 restart app"
+
+# =======================
+
+# export SSHPASS='your-password'
+sshpass -e ssh admin@YOUR-IP "pm2 restart app"
