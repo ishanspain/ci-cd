8eed5a11381473253fbdcab7f1bb8e39bc9c8d06
Ishan Sharma <ishan.spain@gmail.com>
2026-09-23 17:45:37 +0530

    refactor: comment out old commands and streamline file transfer process in vm.sh

--- vm.sh
+++ vm.sh
@@ -14,24 +14,30 @@
 # =======================
 
 
-ssh pcvm "
-sudo -u admin cp -r /home/ubuntu/tests /home/admin/ &&
-sudo -u admin /home/admin/.local/share/pnpm/bin/pm2 restart app
-"
+# ssh pcvm "
+# sudo -u admin cp -r /home/ubuntu/tests /home/admin/ &&
+# sudo -u admin /home/admin/.local/share/pnpm/bin/pm2 restart app
+# "
 
 # =======================
 
 
-PASSWORD='your-password'
+# PASSWORD='your-password'
 
-sshpass -p "$PASSWORD" \
-scp -r ./tests admin@3.6.105.65:/home/admin/
+# sshpass -p "$PASSWORD" \
+# scp -r ./tests admin@3.6.105.65:/home/admin/
 
-sshpass -p "$PASSWORD" \
-ssh admin@3.6.105.65 \
-"/home/admin/.local/share/pnpm/bin/pm2 restart app"
+# sshpass -p "$PASSWORD" \
+# ssh admin@3.6.105.65 \
+# "/home/admin/.local/share/pnpm/bin/pm2 restart app"
 
 # =======================
 
 # export SSHPASS='your-password'
-sshpass -e ssh admin@YOUR-IP "pm2 restart app"
+# sshpass -e ssh admin@YOUR-IP "pm2 restart app"
+
+
+# ===================================
+
+scp -r ./tests pcvm-admin:/home/admin/
+ssh pcvm-admin "/home/admin/.local/share/pnpm/bin/pm2 restart app"
